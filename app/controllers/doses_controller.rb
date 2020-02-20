c33824e449a13ac310eb614e8d3395ff4abdff62
Jennifer Tran <hellojennifertran@gmail.com>
2020-02-20 07:34:44 -0800

    completed doses controller and views...and the rest of the exercise

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,17 +1,30 @@
 class DosesController < ApplicationController
-  before_action :dose_params only %i[create]
+  before_action :dose_params, only: %i[create]
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
+
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.save
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path
   end
 
   private
 
   def dose_params
-    require(:dose).permit(:description, :ingredient)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
